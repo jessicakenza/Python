d08d010503126c3db2c57f777e17ccad8f568b73
Jessica <jessica@manuela.lan>
2024-01-07 14:42:28 +0100

    les bases de python: syntaxe,commentaire,variables,types de données,nombres,chaines,booleans,les listes

--- main.py
+++ main.py
@@ -65,3 +65,93 @@ print(2**3)
 print(5%2)
 print(27//2)
 print((6 + 3) - (6 + 3))
+
+#Listes
+mylist = ["apple", "banana", "cherry"]
+print(len(mylist))
+
+mylist = ["apple", "banana", "cherry"]
+print(mylist[-1])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+print(list1)
+print(list2)
+print(list3)
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+#Modifier un élément d'une liste
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+#Modifier la valeur des élément dans une plage spécifique
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+#Ajouter les éléments à la liste
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+#Supprésion des éléments(remove pour supprimer l'occurence et pop pour supprimer l'index)
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+#Parcourir la liste
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+  thislist = ["apple", "banana", "cherry"]
+  i = 0
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+#Copie de la liste
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+#Tuples
