b4afa0db11739b48f7484a0b11b492e34d0f11c3
Jessica <jessica@manuela.lan>
2024-01-08 10:24:12 +0100

    Les fonctions

--- main.py
+++ main.py
@@ -197,3 +197,44 @@ elif a == b:
       print("a and b are equal")
   else:
       print("a is greater than b")
+
+#Les fonctions
+def my_function():
+  print("Hello from a function")
+my_function()
+
+#Les arguments
+def my_function (fname):
+    print(fname + "Jess")
+
+my_function("Email")
+my_function("Tobias")
+my_function("linus")
+
+def my_function(*kids):
+    print("The youngest child is " +kids[2])
+
+my_function("Emilie" , "Thomas" , "Lary")
+
+#return
+
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+
+#Recursivité
+
+def tri_recursion(k):
+    if(k>0):
+        result = k+ tri_recursion(k - 1)
+        print(result)
+    else:
+        result = 0
+    return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(4)
