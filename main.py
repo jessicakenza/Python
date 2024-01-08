3da8385202e9f0bb8faae6428bacb4468d8523fd
Jessica <jessica@manuela.lan>
2024-01-08 11:20:51 +0100

    Lambda

--- main.py
+++ main.py
@@ -238,3 +238,17 @@ def tri_recursion(k):
 
 print("\n\nRecursion Example Results")
 tri_recursion(4)
+
+#Python Lamda
+x = lambda a : a + 10
+print(x((5)))
+
+x = lambda a, b, c : a + b +c
+print(x(5,6,2))
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
