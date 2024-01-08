9fce2780fdd6fb006e9cc0d208914b282bc35f50
Jessica <jessica@manuela.lan>
2024-01-08 14:32:26 +0100

    itérateurs

---
+++ itérateurs.py
@@ -0,0 +1,22 @@
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+#avec la boucle for
+mystr = "mangues"
+
+for x in mystr:
+  print(x)
