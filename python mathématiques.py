8030922973de3edc96c041b4c5b8744763ed75b3
Jessica <jessica@manuela.lan>
2024-01-08 15:40:45 +0100

    mathématiques

---
+++ python mathématiques.py
@@ -0,0 +1,31 @@
+import math
+
+#Foctions mathématiques intégrées
+x = min(5, 11, 35)
+y = max(5, 10, 25)
+print(x)
+print(y)
+
+#la valeur positive
+x = abs(-7.25)
+print(x)
+
+#calcul des exponentiels
+x = pow(4, 3)
+print(x)
+
+#la racine carrée
+x = math.sqrt(64)
+print(x)
+
+#Arrondissement des nbres
+
+x = math.ceil(3.4)
+y = math.floor(2.5)
+print(x)
+print(y)
+
+#La valeur de pi
+
+x = math.pi
+print(x)
