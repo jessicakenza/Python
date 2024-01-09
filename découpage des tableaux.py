b16ab82efe229dd44397c6b57323493a97516bc8
Jessica <jessica@manuela.lan>
2024-01-09 12:28:45 +0100

    découpage des tableaux avec numpy

---
+++ découpage des tableaux.py
@@ -0,0 +1,10 @@
+import numpy as np
+#Découpage des éléments de l'index 1 à  l'index 5
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5])
+
+#Tranche de l'index 3 de la fin à l'index 1 de la fin :
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[-3:-1])
+
+#étape du découpage
