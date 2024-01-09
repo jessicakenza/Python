fcf0e3d0e7c8bc4fc5b91852599fa8e508cf9e0f
Jessica <jessica@manuela.lan>
2024-01-09 15:33:11 +0100

    concaténation(assemblage des tableaux)

---
+++ Rejoindre des tableauxNumpy.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+#assemnler deux tableaux
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.concatenate((arr1, arr2))
+print(arr)
+
+#assembler des tableaux à l'aide des fonctions de pile
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.stack((arr1, arr2), axis=1)
+print(arr)
+
+#Empilage le long des lignes
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.hstack((arr1, arr2))
+print(arr)
+
+#Empilage le long des colones
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.vstack((arr1, arr2))
+print(arr)
