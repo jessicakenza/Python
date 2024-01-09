d9d6d95db3de464c66533ddbb57301e10d6aa307
Jessica <jessica@manuela.lan>
2024-01-09 16:07:42 +0100

    tri des tableaux avec numpy

---
+++ numPyTriTableaux.py
@@ -0,0 +1,8 @@
+#la fonction utilisée pour trier les tableaux est "sort()"
+
+import numpy as np
+arr = np.array([3, 2, 0, 1])
+print(np.sort(arr))
+
+arr = np.array(['banana', 'cherry', 'apple'])
+print(np.sort(arr))
