9c1f9163e05b112a3eafe2e4bef437017488c844
Jessica <jessica@manuela.lan>
2024-01-11 14:22:51 +0100

    fonctipn distribution du chi-carré

--- distribution du chi-carré.py
+++ distribution du chi-carré.py
@@ -1,1 +1,13 @@
-#
+#elle possède deux parametres df('dégré de liberté) et size
+#Extrayez un échantillon pour la distribution du chi carré avec un degré de liberté 2 et une taille 2x3 :
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+x = random.chisquare(df=2, size=(2, 3))
+print(x)
+
+#visualisation
+sns.distplot(random.chisquare(df=1, size=1000), hist=False)
+plt.show()
