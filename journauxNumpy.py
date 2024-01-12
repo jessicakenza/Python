6d3ff5b20010a640cbf001dc74bf3c7cee6efb34
Jessica <jessica@manuela.lan>
2024-01-12 08:51:36 +0100

    la fonction log2

---
+++ journauxNumpy.py
@@ -0,0 +1,10 @@
+#Utiliser la fonctio  log2
+
+import numpy as np
+arr = np.arange (1, 10)
+print(np.log2(arr))
+
+#Se connecter a la base 10
+import numpy as np
+arr = np.arange(1, 10)
+print(np.log10(arr))
