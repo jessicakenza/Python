f8081b29e7f6a22ba9d2abc5a00ebfc41a69b314
Jessica <jessica@manuela.lan>
2024-01-14 09:03:58 +0100

    gestion des grid avec Matplotlib

---
+++ Matplotlib ajout des lignes de quadrillage.py
@@ -0,0 +1,44 @@
+#ici on utilise la fonction grid()
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("average pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid()
+
+plt.show()
+
+#Specifier les lignes des grilles à afficher : ici on peut utiliser le ârametre 'axi'dans la fonction grid()
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid(axis = 'y')
+
+plt.show()
+
+#definition des propriétés des lignes pour la grille
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
+
+
+
