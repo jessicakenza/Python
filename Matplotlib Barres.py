00b54f59d83382b32a6fc184d430eac94aab6a09
Jessica <jessica@manuela.lan>
2024-01-14 10:24:50 +0100

    Création des barres avec Matplotlib

---
+++ Matplotlib Barres.py
@@ -0,0 +1,23 @@
+#Dessiner 4 barres
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x,y)
+plt.show()
+
+#couleur des barres
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, color = "red")
+plt.show()
+
+#Largeur de la barre
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, width = 0.1)
+plt.show()
